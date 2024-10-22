cf38ee2d6d27f4c7b73165f89706ef726a7af7e4
Wagner <wbnogueira3@gmail.com>
2024-10-22 15:42:04 -0300

    Criação do Header, Input, UserInfo e pages

--- plataforma-dio/src/components/Card/styles.js
+++ plataforma-dio/src/components/Card/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const CardContainer = styled.div`
     width: 100%;
@@ -11,25 +11,31 @@ export const ImageBackground = styled.img`
     width: 100%;
     height: 180px;
 `
-    
+
 export const Content = styled.div`
     width: 100%;
     display: flex;
     flex-direction: column;
     padding: 12px;
+`
+
+export const UserInfo = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 12px;
 
     div {
         margin-left: 12px;
-        }
+    }
 
-    h4 {
+    h4{
         font-family: 'Open Sans';
         font-style: normal;
         font-weight: 700;
         font-size: 18px;
         line-height: 25px;
         color: #FFFFFF;
-        }
+    }
 
     p {
         font-family: 'Open Sans';
@@ -38,16 +44,16 @@ export const Content = styled.div`
         font-size: 12px;
         line-height: 16px;
         color: #FFFFFF;
-        }
+    }
 `
-    
+
 export const UserPicture = styled.img`
     width: 32px;
     height: 32px;
     border-radius: 22px;
-    border: 2px solid #FFFFFF;
+    border: 3px solid #FFFFFF;
 `
-    
+
 export const PostInfo = styled.div`
 
     margin-bottom: 12px;
@@ -89,4 +95,4 @@ export const HasInfo = styled.div`
         font-size: 16px;
         line-height: 22px;
     }
-`;
+`
